a900741977d1ac3b9e2e6fe0b4f3d7c42a793036
Mustafa Ryad <mustafariad03@gmail.com>
2024-11-20 14:44:00 +0200

    Install httpModule

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        timeout: configService.get('HTTP_TIMEOUT') || 5000,
+        maxRedirects: configService.get('HTTP_MAX_REDIRECTS') || 5,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MoviesController],
-  providers: [MoviesService]
+  providers: [MoviesService],
 })
 export class MoviesModule {}

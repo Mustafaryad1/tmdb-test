38c3d115a883a134460ce22cbb7edc997f46fab5
Mustafa Ryad <mustafariad03@gmail.com>
2024-11-20 14:18:34 +0200

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { validateEnv } from './common/helpers/env.validation';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
   try {
+    // Check if all environment variables are set
     validateEnv();
+
+    // Swagger setup
+    const config = new DocumentBuilder()
+      .setTitle('TMDP Test')
+      .setDescription('The TMDP API description for the test')
+      .setVersion('1.0')
+      .addTag('api')
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, documentFactory);
   } catch (error) {
     const logger = new Logger('Bootstrap');
     logger.error(error.message);

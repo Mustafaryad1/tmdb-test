274385f01daececa896f1a1ad65c7a6c59f7a0b5
Mustafa Ryad <mustafariad03@gmail.com>
2024-11-20 14:14:46 +0200

    Add env validation and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env`],
+      isGlobal: true,
+    }),
+    MoviesModule,
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/helpers/env.validation.ts
@@ -0,0 +1,22 @@
+import { Logger } from '@nestjs/common';
+
+export function validateEnv() {
+  const requiredEnvVars = [
+    'TMDB_API_KEY',
+    'TMDB_API_URL',
+    'TMDP_READ_ACCESS_TOKEN',
+    'DATABASE_URL',
+  ];
+  const missingVars = requiredEnvVars.filter((env) => !process.env[env]);
+
+  if (missingVars.length) {
+    const logger = new Logger('EnvironmentValidation');
+    logger.error(`Missing environment variables: ${missingVars.join(', ')}`);
+    throw new Error(
+      `Missing required environment variables: ${missingVars.join(', ')}`,
+    );
+  }
+
+  const logger = new Logger('EnvironmentValidation');
+  logger.log('All required environment variables are set.');
+}
